00481ae5188926ca951e663d6be00c64c68049d2
WillYallop <wyallop14@gmail.com>
2020-09-07 13:29:22 +0100

    Fixed payment functions

--- pages/Campaign/Checkout/_campaignId.vue
+++ pages/Campaign/Checkout/_campaignId.vue
@@ -1,32 +1,47 @@
 <template>
-    <div class="pageContainer">
+    <div class="pageContainer" >
         <div class="horizontalPadding verticalPadding">
-            {{$route.params.campaignId}}
+            <!-- Page Loaded -->
+            <div v-if="pageLoaded" class="checkoutContainer">
+                {{$route.params.campaignId}}
 
-            <form @submit="purchase">
-                <!-- Inputs -->
-                <input class="StripeElement" type="text" placeholder="Email Address" v-model="email"> 
-                <!-- Stripe -->
-                <div ref="cardElement"></div>
-                <!-- Message -->
-                <p v-if="paymentMessage">{{paymentMessage}}</p>
-                <!-- Button -->
-                <button type="submit">Pay</button>
-            </form>
+                <form @submit="purchase">
+                    <!-- Inputs -->
+                    <input class="StripeElement" type="text" placeholder="Email Address" v-model="email"> 
+                    <!-- Stripe -->
+                    <div ref="cardElement"></div>
+                    <!-- Message -->
+                    <p v-if="paymentMessage">{{paymentMessage}}</p>
+                    <!-- Button -->
+                    <button type="submit">Pay</button>
+                </form>
+            </div>
+
+            <div v-else class="pageLoadingContainer">
+                <p>Loading...</p>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
+            // Page Loaded
+            pageLoaded: false,
+            // Stripe
             stripe: null,
             card: null,
+            //Payment Message 
+            paymentMessage: false,
+            // Responce
+            createIntentResponce: null,
             // V-models
             email: null,
-            //Payment Message 
-            paymentMessage: false
 
         }
     },
@@ -38,39 +53,61 @@ export default {
     },
     methods: {
         mountCardInput() {
-            //Assign div stripe checkout input
-            this.stripe = Stripe(process.env.stripePublishableApiKey);
-            const elements = this.stripe.elements();
-            
-            this.card = elements.create('card', {
-                style: {
-                    base: {
-                        iconColor: '#0E1C38',
-                        color: '#0F0F0F',
-                        fontWeight: 500,
-                        fontFamily: 'Lato',
-                        fontSize: '16px',
-                        fontSmoothing: 'antialiased',
-                        ':-webkit-autofill': {
-                            color: '#878787',
-                        },
-                        '::placeholder': {
+            // axios
+            let config = {
+                headers: {
+                    Authorization: this.$auth.getToken('local')
+                }
+            }
+            axios.post(process.env.API_URL + '/payment/create-payment-intent', {
+                campaignId: this.$route.params.campaignId
+            }, config)
+            .then((res) => {
+                
+                this.createIntentResponce = this.res.data
+
+                //Assign div stripe checkout input
+                this.stripe = Stripe(process.env.stripePublishableApiKey);
+                const elements = this.stripe.elements();
+                
+                this.card = elements.create('card', {
+                    style: {
+                        base: {
+                            iconColor: '#0E1C38',
                             color: '#0F0F0F',
+                            fontWeight: 500,
+                            fontFamily: 'Lato',
+                            fontSize: '16px',
+                            fontSmoothing: 'antialiased',
+                            ':-webkit-autofill': {
+                                color: '#878787',
+                            },
+                            '::placeholder': {
+                                color: '#0F0F0F',
+                            },
+                        },
+                        invalid: {
+                            iconColor: '#CA1E1E',
+                            color: '#CA1E1E',
                         },
                     },
-                    invalid: {
-                        iconColor: '#CA1E1E',
-                        color: '#CA1E1E',
-                    },
-                },
-            });
-            this.card.mount(this.$refs.cardElement);
+                });
+                this.card.mount(this.$refs.cardElement);
+
+                this.pageLoaded = true
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+
         },
         purchase(event) {
             event.preventDefault()
             this.$nuxt.$loading.start() // Start loading
 
-            this.stripe.confirmCardPayment(clientSecret, {
+            // complete
+            this.stripe.confirmCardPayment(this.createIntentResponce.clientSecret, {
                 receipt_email: this.email,
                 payment_method: {
                     card: this.card
